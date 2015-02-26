d00a1a1e5d49fea3f9d6a9cca3626f3a8b2de31e
Kat Sullivan <kmsullivan012@gmail.com>
2015-02-26 15:47:23 -0500

    Checksum no longer needs spaces in between bytes, new method to check length of bytes

--- checksum.py
+++ checksum.py
@@ -1,6 +1,5 @@
 def checksum(st):
-	checksum = 0
-	bytes = st.split()
+	bytes = [st[i:i+2] for i in range(0,len(st),2)]
 	checksum = sum([(int(x, 16)) for x in bytes[3:]])
 	checksum = hex(checksum)
 	# Keep only the lowest eight bits
@@ -10,6 +9,12 @@ def checksum(st):
 	checksum = hex(int('0xff', 16) - int(lowest_eight_bits, 16))
 	print checksum
 
+def length(st):
+	bytes = [st[i:i+2] for i in range(0,len(st),2)]
+	print hex(len(bytes))
+
 st = "7E 00 10 17 01 00 13 A2 00 40 97 56 B8 FF FE 02 42 52 01"
-whatWeWant = "7E 00 10 17 01 00 13 A2 00 40 97 56 B8 FF FE 02 42 52 01"
+whatWeWant = "7E001017010013A200409756B8FFFE02425201"
+withoutLength = "17010013A200409756B8FFFE02425201"
 checksum(whatWeWant)
+length(withoutLength)
