bd0849fec042f49da5317e00006850b0351a87c5
dmelancon <dmelancon.d@gmail.com>
2015-02-26 16:25:58 -0500

    updated FrameSend

--- frameSend.py
+++ frameSend.py
@@ -14,31 +14,42 @@ radioDict = {
 	# Kat
 	"radio3": '0013A20040975703'
 }
+
 def messageToHex(message):
 	return binascii.hexlify(message)
 
-def frameLength():
-	length = 13 + len(messageToHex(message))/2
-	hexlen = '00'+ hex(length)[2:4]
-	return hex(length)
+def checksum(st):
+	bytes = [st[i:i+2] for i in range(0,len(st),2)]
+	checksum = sum([(int(x, 16)) for x in bytes[3:]])
+	checksum = hex(checksum)
+	# Keep only the lowest eight bits
+	lowest_eight_bits = [i for i in str(checksum[-2:])]
+	lowest_eight_bits = ''.join(lowest_eight_bits)
+	# Subtract this from 0xff
+	checksum = hex(int('0xff', 16) - int(lowest_eight_bits, 16))
+	return str(checksum)[2:]
 
-transmitRequest = '7E' + frameLength() + '1001' + radioDict[radio] + 'FFFE'+'00'+ messageToHex(message) 
-# + checksum()
-print transmitRequest
+def length(st):
+	bytes = [st[i:i+2] for i in range(0,len(st),2)]
+	length_hex = "{0:#0{1}x}".format(len(bytes),6)
+	return length_hex[2:]
 
 def syntax_thing(str):
-       new_str = ""
-       for idx,char in enumerate(str):
-               if idx%2==1:
-                       new_str += ("\\x" + str[idx-1] + str[idx])
-       return new_str
-
-ser = serial.Serial('/dev/cu.usbserial', 9600, timeout=2)
- 
-
-# ser.write(tx)
- 
-# s = ser.read(20)
-# print s 
+	new_str = ""
+	for idx,char in enumerate(str):
+		if idx%2==1:
+			new_str += ("\\x" + str[idx-1] + str[idx])
+	return new_str
 
 
+transmitRequest = '1001' + radioDict[radio] + 'FFFE' + '0000' + messageToHex(message)
+transmitRequest = '7E' + length(transmitRequest) + transmitRequest
+transmitRequest = transmitRequest + checksum(transmitRequest)
+print transmitRequest
+transmitRequest = syntax_thing(transmitRequest);
+
+ser = serial.Serial(port, 9600, timeout=2)
+ser.write(transmitRequest)
+s = ser.read(50)
+print transmitRequest
+print binascii.hexlify(s) 
