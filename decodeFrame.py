7831f55943f756293b444f46a3e1d9d36c41a2ad
dmelancon <dmelancon.d@gmail.com>
2015-03-02 12:00:48 -0500

    added decodeFrame

---
+++ decodeFrame.py
@@ -0,0 +1,23 @@
+#this function decodes AT Command Response
+def decodeAtCommandResponse(response):
+	responseType = {
+		'00' : 'OK',
+		'01' : 'ERROR',
+		'02' : "INVALID COMMAND",
+		'03' : "INVALID PARAMETER"
+	}
+	response = response[-6:-4]
+	return responseType[response]
+
+#this function decodes both Transmit Request and Remote AT Command Responses
+def decodeResponse(response):
+	responseType = {
+		'00' : 'OK',
+		'01' : 'ERROR',
+		'02' : "INVALID COMMAND",
+		'03' : "INVALID PARAMETER"
+	}
+	response = response[-4:-2]
+	return responseType[response]
+
+
