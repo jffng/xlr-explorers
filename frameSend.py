4bacb02ef1cf08269bf8e4ddddf5f41e7bb1f2a7
Kat Sullivan <kmsullivan012@gmail.com>
2015-03-02 10:42:38 -0500

    Cleaned up frameSend.py, got rid of unused and commented out lines of code

--- frameSend.py
+++ frameSend.py
@@ -1,17 +1,18 @@
-# transmit request: hello world to radio 1
+##################################################
+## Transmit Request: hello world to radio 1
+## By: Dan Melancon, Jeff Ong, and Kat Sullivan
+##################################################
 import serial
 import sys
 import binascii
+
 port = sys.argv[1]
 radio = sys.argv[2]
 message = sys.argv[3]
 
 radioDict = {
-	# Dan
 	"radio1": '0013A200409756B8',
-	# Jeff
 	"radio2": '0013A200409756BD',
-	# Kat
 	"radio3": '0013A20040975703'
 }
 
@@ -34,27 +35,14 @@ def length(st):
 	length_hex = '{0:#0{1}x}'.format(len(bytes),6)
 	return length_hex[2:]
 
-def syntax_thing(str):
-	new_str = ''
-	for idx,char in enumerate(str):
-		if idx%2==1:
-			new_str += (chr(0x5c) + 'x' + str[idx-1].upper() + str[idx].upper())
-	return new_str
-
 transmitRequest = '1001' + radioDict[radio] + 'FFFE' + '0000' + messageToHex(message)
 transmitRequest = '7E' + length(transmitRequest) + transmitRequest
 transmitRequest = transmitRequest + checksum(transmitRequest)
-# print transmitRequest
-# print transmitRequest.decode('hex')
-# print binascii.unhexlify(transmitRequest)
-# binHEx = bin(int(transmitRequest,16))[2:]
-# print hex(int(binHEx,2))
-# transmitRequest = syntax_thing(transmitRequest);
 
 tx = transmitRequest
 ser = serial.Serial(port, 9600, timeout=2)
 ser.write(transmitRequest.decode("hex"))
-#ser.write("b"+tx)
+
 print transmitRequest.decode("hex")
 s = ser.read(50)
 print binascii.hexlify(s) 
