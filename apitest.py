7f5e1d6c1e22dc9c9eec6e010c465c1f768df815
Jeff Ong <jonger4@gmail.com>
2015-03-05 21:19:30 -0500

    spacing issue

--- apitest.py
+++ apitest.py
@@ -5,7 +5,6 @@ ser = serial.Serial('/dev/cu.usbserial', 9600, timeout=2)
 # init a new remote AT command to change the target radio's data rate to the 2nd setting.
 # call update() to generate its data frame.
 # pass it the serial object and number of bytes to expect from the response.
-
 remote_at_command = xlr.RemoteAT('br02', 'radio1')
 remote_at_command.update()
 remote_at_command.send(ser, 50)
@@ -13,7 +12,6 @@ remote_at_command.send(ser, 50)
 # init a transmit request, 'hello world'.
 # call update() to generate its data frame.
 # pass it the serial object and number of bytes to expect from the response.
-
 transmit_request = xlr.Transmit('hello world', 'radio1')
 transmit_request.update()
 transmit_request.send(ser, 50)
